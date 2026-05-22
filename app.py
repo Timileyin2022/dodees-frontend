b8977df718d0f73aa8530b1c2637994ebaef7336
Timileyin2022 <akinkuotutimileyinmatthew@gmail.com>
2026-05-22 13:21:52 +0100

    Fixed Render port binding

--- app.py
+++ app.py
@@ -400,4 +400,6 @@ def investors_api():
     })
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
